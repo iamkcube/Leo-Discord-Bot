c657a8e9ab3f3c8edf96b1d42c610640121188dc
Kalinga <kalingakhatua@gmail.com>
2021-11-05 10:25:42 +0530

    status change for Leo

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
 help_command = commands.DefaultHelpCommand(no_category = 'All Commands:')
 
-myleo = commands.Bot(command_prefix="-",help_command = help_command , activity=discord.Activity(type=discord.ActivityType.listening, name="With You! :heart:")  )
+myleo = commands.Bot(command_prefix="-",help_command = help_command , activity=discord.Activity(type=discord.ActivityType.listening, name="Music With You! 💟")  )
 
 
 
@@ -99,7 +99,7 @@ myleo = commands.Bot(command_prefix="-",help_command = help_command , activity=d
 
 myqueue=[]
 
-allqueue={}
+allqueue=[]
 
 nowplaying = ""
 
